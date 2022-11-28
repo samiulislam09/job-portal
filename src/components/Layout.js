c6ea6719a152a265a33ecef39b07e0f7d1f28088
samiulislam09 <samiulislamsawon09@gmail.com>
2022-11-28 17:34:52 +0600

    first  commit

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/Editjob.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Editjob() {
+  return <div>Editjob</div>;
+}
+
+export default Editjob;
